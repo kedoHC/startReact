231cf923b37a427245adf7c6a7e6efb9ca0e59df
Jesús  Durán <jesusduran@Jesuss-Mac-mini.local>
2018-05-18 16:43:40 -0500

    primer commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './css/App.css';
+import Header from './js/components/Header'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="main-container">
+      	<Header />
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/js/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../../css/components/header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Empieza...</h1>
+
+      </div>
+    );
+  }
+}
+
+export default Header;
